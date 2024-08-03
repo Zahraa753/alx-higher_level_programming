0a280f199f09760272d6dd4dd0cb4dcc18ef61ff
Zahraa753 <zahraahassan075@gmail.com>
2024-08-03 15:47:10 +0300

    check if has cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if my list has circle
+ * @list : pointer to list to check
+ * Return: 1 if has cicle, 0 if not
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t slow = list, *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
